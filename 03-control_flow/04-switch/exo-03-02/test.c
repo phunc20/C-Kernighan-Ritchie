61c7e5305cc721fbd2801f474b2a308a3ade443c
phunc20 <wucf20@gmail.com>
2021-01-07 10:49:26 +0700

    exo-03-02 DONE

--- 03-control_flow/04-switch/exo-03-02/test.c
+++ 03-control_flow/04-switch/exo-03-02/test.c
@@ -4,8 +4,15 @@
 int main(int argc, char **argv) {
   char s[] = "Tab is  \t, newline is \nBut escape like \\n and \\t don't count\nYeah\tyooh!\n";
   printf("s = %s\n", s);
+
   char t[sizeof(s)/sizeof(char)];
   escape(s, t);
   printf("t = %s\n", t);
+  printf("\n");
+
+  char ss[sizeof(s)/sizeof(char)];
+	epacse(ss, t);
+  printf("ss = %s\n", ss);
+
   return 0;
 }
--- 03-control_flow/04-switch/exo-03-02/utils.c
+++ 03-control_flow/04-switch/exo-03-02/utils.c
@@ -1,10 +1,9 @@
-
+/* think of t as the return value: t is almost identical
+ * to s, except that \n, \t, \\ are replced by symbols
+ */
 void escape(char s[], char t[]) {
   int i, j;
-  //len_s = sizeof(s) / sizeof(char);
-  //len_t = sizeof(t) / sizeof(char);
-  //char c;
-  //for (c=s[i=0]; c!='\0'; ++i)
+	// i and j are going to be indices of s and t, resp.
   j = 0;
   for (i=0; s[i]!='\0'; ++i) {
     switch (s[i]) {
@@ -28,32 +27,35 @@ void escape(char s[], char t[]) {
   t[j] = '\0';
 }
 
+/* epacse() does the reverse of escape(), transforming
+ * t back to the original s.
+ */
 void epacse(char s[], char t[]){
   int i, j;
-  //len_s = sizeof(s) / sizeof(char);
-  //len_t = sizeof(t) / sizeof(char);
-  //char c;
-  //for (c=s[i=0]; c!='\0'; ++i)
-  j = 0;
-  for (i=0; s[i]!='\0'; ++i) {
-    switch (s[i]) {
-      case '\n' :
-        t[j++] = '\\';
-        t[j++] = 'n';
-        break;
-      case '\t':
-        t[j++] = '\\';
-        t[j++] = 't';
-        break;
-      case '\\':
-        t[j++] = '\\';
-        t[j++] = '\\';
-        break;
-      default:
-        t[j++] = s[i];
-        break;
-    }
+	// i and j are going to be indices of s and t, resp.
+  for (i=j=0; t[j]!='\0'; j++) {
+		if (t[j] == '\\') {
+			j++;
+      switch (t[j]) {
+        case 'n' :
+          s[i++] = '\n';
+          break;
+        case 't':
+          s[i++] = '\t';
+          break;
+				case '\\':
+          s[i++] = '\\';
+          break;
+        //default:
+        //  s[i++] = '\\';
+        //  s[i++] = t[j];
+        //  break;
+      }
+		}
+		else {
+			s[i++] = t[j];
+		}
   }
-  t[j] = '\0';
 
+  s[i] = '\0';
 }
